09e1225f842c1a4f9e4ed5759b60fc989c9e4099
Ronnapon <ronnapon1992@live.com>
2021-07-23 20:20:13 +0700

    AddFirstController

---
+++ my-cars/Controllers/CarsController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using my_cars.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace my_cars.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CarsController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult AddDoctor([FromBody] Car car)
+        {
+            return Ok();
+        }
+
+        [HttpGet]
+        public IActionResult GetAllDoctors(string search, string specialization, int page)
+        {
+            return Ok();
+        }
+
+        [HttpGet("{Id}")]
+        public IActionResult GetDoctor(int Id)
+        { 
+            return Ok();
+        }
+
+        [HttpPut("{Id}")]
+        public IActionResult UpdateDoctor([FromBody] Car car, int Id)
+        {      
+            return Ok();
+        }
+
+        [HttpDelete]
+        public IActionResult DeleteDoctor(int Id)
+        {
+            return Ok();
+        }
+    }
+}
